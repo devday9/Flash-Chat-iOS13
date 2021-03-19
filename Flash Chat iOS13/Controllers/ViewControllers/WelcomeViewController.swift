fb6c6fa49346ed895c339623783c77a8f4ab11eb
Dev <devenlukeday@gmail.com>
2021-03-19 10:42:01 -0600

    adds pod file and firebase pods

--- Flash Chat iOS13/Controllers/ViewControllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/ViewControllers/WelcomeViewController.swift
@@ -9,15 +9,30 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
-
+    
     //MARK: - Outlets
     @IBOutlet weak var titleLabel: UILabel!
     
     //MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        setupTitleLabelAnimation()
     }
     
-
+    //MARK: - Helper Functions
+    func setupTitleLabelAnimation() {
+    
+        titleLabel.text = ""
+        var charIndex = 0.0
+        let titleText = "⚡️FlashChat"
+        for letter in titleText {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
+                self.titleLabel.text?.append(letter)
+            }
+            charIndex += 1
+        }
+    }
+    
+    
 }//END OF CLASS
