a288f7268ad920e452d92abb647c3439622a33bf
Dev <devenlukeday@gmail.com>
2021-03-22 15:33:18 -0600

    adds loadmessage function to load messages from firestore and keeps them in order

--- Flash Chat iOS13/Controllers/ViewControllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ViewControllers/ChatViewController.swift
@@ -15,15 +15,21 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    //MARK: - Properties
+    var messages: [Message] = []
+    let db = Firestore.firestore()
+    
     //MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
         setupViews()
     }
     
     //MARK: - Actions
     @IBAction func sendPressed(_ sender: UIButton) {
-        
+        sendMessage()
     }
     
     @IBAction func logOutTapped(_ sender: Any) {
@@ -31,6 +37,25 @@ class ChatViewController: UIViewController {
     }
     
     //MARK: - Helper Functions
+    func setupViews() {
+        hidesBackButton()
+        setupTitle()
+        setupCustomCell()
+        loadMessages()
+    }
+    
+    func setupTitle() {
+        title = K.appName
+    }
+    
+    func hidesBackButton() {
+        navigationItem.hidesBackButton = true
+    }
+    
+    func setupCustomCell() {
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
+    }
+    
     func logOutUser() {
         do {
             try Auth.auth().signOut()
@@ -41,16 +66,64 @@ class ChatViewController: UIViewController {
         }
     }
     
-    func setupViews() {
-        hidesBackButton()
-        setupTitle()
+    func sendMessage() {
+        
+        if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email {
+            db.collection(K.FStore.collectionName).addDocument(data: [
+                K.FStore.senderField : messageSender,
+                K.FStore.bodyField : messageBody,
+                K.FStore.dateField : Date().timeIntervalSince1970
+            ]) { (error) in
+                if let error = error {
+                    print("Error in \(#function) : \(error.localizedDescription) \n---\n \(error)")
+                } else {
+                    print("Successfully saved data")
+                }
+            }
+        }
     }
     
-    func setupTitle() {
-        title = K.appName
-    }
-    
-    func hidesBackButton() {
-        navigationItem.hidesBackButton = true
+    func loadMessages() {
+        
+        db.collection(K.FStore.collectionName)
+            .order(by: K.FStore.dateField)
+            .addSnapshotListener { (querySnapshot, error) in
+                
+                self.messages = []
+                
+                if let error = error {
+                    print("Error in \(#function) : \(error.localizedDescription) \n---\n \(error)")
+                } else {
+                    if let snapshotDocuments = querySnapshot?.documents {
+                        for doc in snapshotDocuments {
+                            let data = doc.data()
+                            if  let messageSender = data[K.FStore.senderField] as? String,
+                                let messageBody = data[K.FStore.bodyField] as? String {
+                                let newMessage = Message(sender: messageSender, body: messageBody)
+                                self.messages.append(newMessage)
+                                
+                                DispatchQueue.main.async {
+                                    self.tableView.reloadData()
+                                }
+                            }
+                        }
+                    }
+                }
+            }
     }
 }//END OF CLASS
+
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as? MessageCell else { return UITableViewCell() }
+        
+        cell.label.text = messages[indexPath.row].body
+        
+        return cell
+    }
+}//END OF EXTENSION
---
+++ Flash Chat iOS13/Views/MessageCell.swift
@@ -0,0 +1,35 @@
+//
+//  MessageCell.swift
+//  Flash Chat iOS13
+//
+//  Created by Deven Day on 3/22/21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+    
+    //MARK: - Outlets
+    @IBOutlet weak var messageBubble: UIView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    //MARK: - Lifecycle Methods
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupViews()
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+    //MARK: - Helper Functions
+    func setupViews() {
+        messageBubble.layer.cornerRadius = messageBubble.frame.size.height / 5
+    }
+    
+}//END OF CLASS
